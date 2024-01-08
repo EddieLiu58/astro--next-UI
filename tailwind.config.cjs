f580cbf1aa7ad527e482b256e1f0ba928f4cc097
EddieLiu <eddieliu58@gmail.com>
2024-01-08 13:25:20 +0800

    feat: default theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,8 +7,19 @@ export default {
 		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: { white: "#FFFFFF", black: "#000000", focus: "#BEF264" },
+		},
 	},
 	darkMode: "class",
-	plugins: [nextui()],
+	plugins: [
+		nextui({
+			addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+			defaultTheme: "light", // default theme from the themes object
+			layout: {
+				spacingUnit: 4, // in px
+			},
+		}),
+	],
 };
+``;
